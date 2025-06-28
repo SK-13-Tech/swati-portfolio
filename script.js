abc535a69abd9a90a9acbdb605ae95db299a78ca
Swati <swati.k1303@gmail.com>
2025-06-28 12:44:22 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
-  alert('Thank you for reaching out!');
+  alert('Thank you for reaching out!!');
 });
