01abefb912dd808885f29f3f565a49efd17f6bed
Swati <swati.k1303@gmail.com>
2025-09-06 17:36:57 +0530

    New Portfolio boilerplate

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thank you for reaching out!!');
+// Mobile menu toggle
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("navbar");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
 });
